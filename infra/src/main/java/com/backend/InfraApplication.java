7b71dcf2a00a5f94d7e1b04d046446f93a33697a
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-10-09 01:10:29 +0300

    Updated pom.xml and small changes

--- infra/src/main/java/com/backend/InfraApplication.java
+++ infra/src/main/java/com/backend/InfraApplication.java
@@ -3,7 +3,12 @@ package com.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration.class
+        }
+)
 public class InfraApplication {
     public static void main(String[] args) {
         SpringApplication.run(InfraApplication.class, args);
